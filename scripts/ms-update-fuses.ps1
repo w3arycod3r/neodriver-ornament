0826223d39a34dfb2187a882775ef0e335cf759e
Garrett Carter <w3arycod3r@outlook.com>
2024-04-27 16:17:04 -0500

    add param and robustness

--- scripts/ms-update-fuses.ps1
+++ scripts/ms-update-fuses.ps1
@@ -1,11 +1,17 @@
 # File:    ms-update-fuses.ps1
 # Author:  Garrett Carter
 # Purpose: Update fuses if needed using atprogram.exe and the fuses.txt file
+param (
+    [Parameter(Mandatory)]
+    [string]
+    $desired_fuses_file
+)
 
-$Env:PATH = "C:\Program Files (x86)\Atmel\Studio\7.0\;C:\Program Files (x86)\Atmel\Studio\7.0\shellutils\;C:\Program Files (x86)\Atmel\Studio\7.0\atbackend\;C:\Program Files (x86)\Atmel\Studio\7.0\toolchain\avr8\avr8-gnu-toolchain\bin\;$Env:PATH"
-$curr_fuses_file = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\microchip-studio\curr_fuses.txt")
-$desired_fuses_file = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\microchip-studio\fuses.txt")
 $ErrorActionPreference = 'Stop'
+$Env:PATH = "C:\Program Files (x86)\Atmel\Studio\7.0\;C:\Program Files (x86)\Atmel\Studio\7.0\shellutils\;C:\Program Files (x86)\Atmel\Studio\7.0\atbackend\;C:\Program Files (x86)\Atmel\Studio\7.0\toolchain\avr8\avr8-gnu-toolchain\bin\;$Env:PATH"
+$curr_fuses_file = [System.IO.Path]::GetFullPath("$PSScriptRoot\curr_fuses.txt")
+$desired_fuses_file = [System.IO.Path]::GetFullPath($desired_fuses_file)
+$desired_fuses_settings = Get-Content $desired_fuses_file
 
 # Use verbose flag for atprogram commands? 1 for yes, 0 for no
 $atprog_use_verbose = 0
@@ -24,8 +30,10 @@ if ($LASTEXITCODE -ne 0) {
     Exit $LASTEXITCODE
 }
 
-$desired_fuses_settings = Get-Content $desired_fuses_file
 $curr_fuses_settings = Get-Content $curr_fuses_file
+if (Test-Path $curr_fuses_file) {
+    Remove-Item -Force $curr_fuses_file
+}
 
 if ($curr_fuses_settings -eq $desired_fuses_settings) {
     Write-Host "Fuses set correctly to 0x$curr_fuses_settings"
@@ -38,10 +46,6 @@ if ($curr_fuses_settings -eq $desired_fuses_settings) {
     if ($LASTEXITCODE -ne 0) {
         Exit $LASTEXITCODE
     }
-}
-
-if (Test-Path $curr_fuses_file) {
-    Remove-Item -Force $curr_fuses_file
 }
 
 Exit 0
