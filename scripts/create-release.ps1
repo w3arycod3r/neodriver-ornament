53178942ab9c9f5433795b98073577a66f2b47b0
Garrett Carter <w3arycod3r@outlook.com>
2024-04-28 18:03:41 -0500

    add release scripts

---
+++ scripts/create-release.ps1
@@ -0,0 +1,29 @@
+# File:    create-release.ps1
+# Author:  Garrett Carter
+# Purpose: Package hex files, fuse settings, and scripts into a zip for release
+
+$releases_dir = "$PSScriptRoot\..\releases"
+if (-Not (Test-Path $releases_dir)) {
+    New-Item -Path $releases_dir -ItemType Directory | Out-Null
+}
+
+$release_git_rev = $(git describe --exact-match --tags 2>$null || git rev-parse --short HEAD)
+$release_zip_name = "neodriver-ornament-$release_git_rev.zip"
+$release_zip_file = "$releases_dir\$release_zip_name"
+$release_zip_file = [System.IO.Path]::GetFullPath($release_zip_file)
+
+Write-Host "Creating release at $release_zip_file ..."
+
+$params = @{
+    Path = "$PSScriptRoot\..\microchip-studio\neo_driver_app\eep_data_write\eep_data_write.hex",
+           "$PSScriptRoot\..\microchip-studio\neo_driver_app\Release\neo_driver_app.hex",
+           "$PSScriptRoot\..\microchip-studio\fuses.txt",
+           "$PSScriptRoot\..\scripts\ms-update-fuses.ps1",
+           "$PSScriptRoot\..\scripts\ms-flash-hex.ps1",
+           "$PSScriptRoot\..\scripts\ms-release-sequence.ps1"
+    CompressionLevel = "Fastest"
+    DestinationPath = "$release_zip_file"
+    Force = $true
+}
+
+Compress-Archive @params
