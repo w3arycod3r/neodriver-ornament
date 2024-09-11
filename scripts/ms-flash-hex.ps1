92393a6836f425f72c0919a61d22ec57299e74a7
Garrett Carter <w3arycod3r@outlook.com>
2024-09-10 22:10:14 -0500

    add time based recovery

--- scripts/ms-flash-hex.ps1
+++ scripts/ms-flash-hex.ps1
@@ -14,6 +14,9 @@ param (
     $max_flash_attempts = 2
 )
 
+# If the program step takes longer than this, the tool is likely in a bad state and we should recover it.
+$max_flash_time_sec = 10
+
 $ErrorActionPreference = 'Stop'
 if (-Not (Get-Command atprogram.exe -ErrorAction SilentlyContinue)) {
     Write-Host "Adding Atmel Studio dirs to process PATH variable..."
@@ -34,6 +37,7 @@ if ($atprog_use_verbose -eq 1){
 $num_flash_attempts = 1
 $flash_success = $false
 $last_attempt = $false
+$stop_watch = New-Object -TypeName 'System.Diagnostics.Stopwatch'
 while ($num_flash_attempts -le $max_flash_attempts) {
 
     if ($num_flash_attempts -eq $max_flash_attempts) {
@@ -43,7 +47,10 @@ while ($num_flash_attempts -le $max_flash_attempts) {
     Write-Host "Flashing $hex_to_flash (Attempt $num_flash_attempts)"
     $atprogram_command = "atprogram.exe $atprog_verbose --force -t atmelice -i ISP -d attiny85 --externalreset program --chiperase -f $hex_to_flash --verify"
     Write-Host $atprogram_command
+    $stop_watch.Reset()
+    $stop_watch.Start()
     Invoke-Expression $atprogram_command
+    $stop_watch.Stop()
     
     if ($LASTEXITCODE -ne 0) {
         # If we failed to flash, try to recover the tool and target before trying again.
@@ -64,6 +71,13 @@ while ($num_flash_attempts -le $max_flash_attempts) {
 if (-Not $flash_success) {
     Write-Host "Failed to flash $hex_to_flash after $max_flash_attempts attempts"
     Exit 1
+}
+
+$last_flash_time = $stop_watch.Elapsed.TotalSeconds
+Write-Host "Flashing took $last_flash_time seconds"
+if ($last_flash_time -gt $max_flash_time_sec) {
+    Write-Host "Flashing took longer than $max_flash_time_sec seconds. The tool may be in a bad state."
+    & $PSScriptRoot\ms-recover-tool.ps1 -desired_fuses_file $desired_fuses_file
 }
 
 Exit 0
